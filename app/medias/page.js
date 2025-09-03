79fe76ff84ff1dae7a35620b726142b28d5478db
mey yaou <meynedjmadehbiyayaou@gmail.com>
2025-09-03 14:43:34 +0100

    update

--- app/medias/page.js
+++ app/medias/page.js
@@ -31,7 +31,7 @@ function Page() {
         ))}
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {filtered.map((item, idx) => (
           <div key={idx}>
             {item.type === 'images' ? (
