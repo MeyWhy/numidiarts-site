14945f45d9a08328de49bb260df47430535fdc88
mey yaou <meynedjmadehbiyayaou@gmail.com>
2025-10-09 13:22:51 +0100

    fix links for navigation

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,14 +1,13 @@
 import Link from 'next/link'
-import { withBase } from '@/utils/basePath'
 import React from 'react'
 function Footer() {
   return (
     <div>
 <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
   <nav className="grid grid-flow-col gap-4">
-    <Link href={withBase('/about')} className="link link-hover">L&apos;association</Link>
-    <Link href={withBase('/contact')} className="link link-hover">Contact</Link>
-    <Link href={withBase('/medias')} className="link link-hover">Médias</Link>
+    <Link href='/about' className="link link-hover">L&apos;association</Link>
+    <Link href='/contact' className="link link-hover">Contact</Link>
+    <Link href='/medias' className="link link-hover">Médias</Link>
   </nav>
 
   <aside>
--- utils/basePath.js
+++ utils/basePath.js
@@ -1,4 +1,5 @@
 export function withBase(path) {
   const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  console.log("prefix is ",prefix);
   return `${prefix}${path}`;
 }
