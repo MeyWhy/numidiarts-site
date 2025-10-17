92adc5caebcac13b9171d0e11682b256f8466bdf
mey yaou <meynedjmadehbiyayaou@gmail.com>
2025-10-17 20:52:20 +0100

    deploy with correct asset pathy

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
 const nextConfig = {
   output: 'export', 
   images: { unoptimized: true },
-  basePath: '/numidiarts-site',
-  assetPrefix: '/numidiarts-site/',
+
    env: {
-    NEXT_PUBLIC_BASE_PATH: '/numidiarts-site',
+        NEXT_PUBLIC_BASE_PATH: '' ,
   },
 };
 
